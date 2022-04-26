222643cb8dfcb74ac07e75914bcd04abfcfa4464
GRND0 <zefiro.turchetta@gmail.com>
2022-04-26 18:49:59 +0200

    aggiunte funzioni principali

--- script/script.js
+++ script/script.js
@@ -1,100 +1,100 @@
-/*const selettore = parseInt(document.getElementById("livello").value) ;
-const arrayCampo = numeratore(selettore) ;
-const numeroBombe = 16 ;
-const limiteMassimo = selettore - numeroBombe ;
-const arrayBombe = numeriRandomUnici(numeroBombe, limiteMassimo) ;
-const celleGiuste = [] ;*/
 
-const tastoStart = document.getElementById("play") ;
-tastoStart.addEventListener("click", inizioGioco) ;
+
+const tastoStart = document.getElementById("play");
+tastoStart.addEventListener("click", inizioGioco);
 
 
 function inizioGioco() {
-    const invito = document.getElementById("invito-gioco") ;
-    const contenitore = document.getElementById("contenitore-griglia") ;
-    invito.classList.add("nascosto") ;
-    contenitore.classList.remove("nascosto") ;
-    contenitore.innerHTML = "" ;
+    const invito = document.getElementById("invito-gioco");
+    const contenitore = document.getElementById("griglia");
+    invito.classList.add("nascosto");
+    contenitore.classList.remove("nascosto");
+    contenitore.innerHTML = "";
 
-    const selettore = parseInt(document.getElementById("livello").value) ;
-    let numeroCelle ;
-    let cellePerRiga ;
+
+
+    const selettore = parseInt(document.getElementById("livello").value);
+    let numeroCelle;
+    let cellePerRiga;
     if (selettore === 1) {
-        numeroCelle = 100 ;
-        cellePerRiga = 10 ;
-    } else if  (livello === 2) {
-        numeroCelle === 81 ;
-        cellePerRiga = 9 ;
+        numeroCelle = 100;
+        cellePerRiga = 10;
+    } else if (selettore === 2) {
+        numeroCelle === 81;
+        cellePerRiga = 9;
     } else {
-        numeroCelle === 49 ;
-        cellePerRiga = 7 ;
+        numeroCelle === 49;
+        cellePerRiga = 7;
     }
 
+    const numeroBombe = 16;
+    const arrayBombe = generatoreNumeriUnici(numeroBombe, numeroCelle);
+    const celleGiuste = [];
+    const condizioneVittoria = numeroCelle - numeroBombe ;
+
+
     for (let i = 1; i <= numeroCelle; i++) {
-        const nuovoOggetto = generatoreBox(i, cellePerRiga) ;
-        nuovoOggetto.addEventListener("click", gestioneClick)
-        contenitore.append(nuovoOggetto) ; 
+        const nuovoOggetto = generatoreBox(i, cellePerRiga);
+        nuovoOggetto.addEventListener("click", gestioneClick);
+        contenitore.append(nuovoOggetto);
     }
 
 }
-
-/*const contenitoreGriglia = document.querySelector(".contenitore-griglia");
-for (let i = 0; i < arrayCampo.length; i++) {
-    const numeroProgressivo = arrayCampo[i];
-
-    const elementoHtml = document.createElement("div");
-
-    elementoHtml.innerHTML = `<span>${numeroProgressivo}</span>`;
-
-    elementoHtml.classList.add("box-griglia");
-
-    elementoHtml.addEventListener("click", function () {
-        this.classList.add("attivo")
-    });
-
-    contenitoreGriglia.append(elementoHtml);
-}*/
-
-
-
-
 
 //FUNZIONI
 
 function generatoreBox(numeroInterno, cellePerRiga) {
-    const elementoHtml = document.createElement("div") ;
-    elementoHtml.classList.add("box-griglia") ;
-    elementoHtml.style.width = `calc(100% / ${cellePerRiga})` ;
-    elementoHtml.style.height = `calc(100% / ${cellePerRiga})` ;
+    const elementoHtml = document.createElement("div");
+    elementoHtml.classList.add("box-griglia");
+    elementoHtml.style.width = `calc(100% / ${cellePerRiga})`;
+    elementoHtml.style.height = `calc(100% / ${cellePerRiga})`;
     elementoHtml.innerHTML = `<span>${numeroInterno}</span>`;
-    return elementoHtml ;
+    return elementoHtml;
 
 }
 
 function gestioneClick() {
-    this.classList.add("attivo") ;
-
-}
-
-/*function numeratore(numeroGriglia) {
-    const arrayCifre = [];
-    for (let i = 1; i <= numeroGriglia; i++) {
-        arrayCifre.push(i);
-    }
-    return arrayCifre;
-}
-
-function numeroRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min ;
-}
-
-function numeriRandomUnici(numeroCifre, limiteMassimo) {
-    const arrayRandom = [] ;
-    while (arrayRandom.length < numeroCifre) {
-        const numeroCasuale = numeroRandom(1, limiteMassimo);
-        if (!arrayRandom.includes(numeroCasuale)) {
-            arrayRandom.push(numeroRandom);
+    const numeroScelto = parseInt(this.querySelector("span").textContent);
+    if (arrayBombe.includes(numeroScelto)) {
+        this.classList.add("rosso");
+        fineGioco(celleGiuste.length, "perso") ;
+    } else {
+        this.classList.add("attivo");
+        this.style.pointerEvents = "none";
+        celleGiuste.push(numeroScelto);
+        if (celleGiuste.length >= condizioneVittoria) {
+            fineGioco(celleGiuste.length, "vinto")
         }
+
     }
-    return arrayRandom ;
-}*/
+
+function fineGioco(numeriGiusti, vintoPerso) {
+    document.getElementById("risultato") ;
+    let messaggio ; 
+    if (vintoPerso === "perso") {
+        messaggio = `Hai perso! Hai indovinato ${numeriGiusti} numeri`
+} else {
+    messaggio = "Hai vinto!"
+    document.getElementById("risultato").innerHTML = messaggio ;
+}
+
+}
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+function generatoreNumeriUnici(bombe, limite) {
+    const arrayNumeri = [];
+    while (arrayNumeri.length < bombe) {
+        const numeroRandom = getRndInteger(1, limite);
+        if (!arrayNumeri.includes(numeroRandom)) {
+            arrayNumeri.push(numeroRandom);
+        }
+
+    }
+    return arrayNumeri;
+}
